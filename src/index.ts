1b0eda84cadcaf5eb774bdd9e8bfa93f968818cc
Frederik Bolding <frederik.bolding@gmail.com>
2024-11-12 22:51:13 +0100

    Use tables for output

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,66 @@
-import { SCHEDULE_2024 as SCHEDULE } from "./data";
+import { Conference, SCHEDULE_2024 as SCHEDULE, TEAM_MAP, Team } from "./data";
 import { getSeeding } from "./playoff";
 import { calculatePlayoffProbability } from "./probability";
+import Table from "cli-table3";
 
-console.log(getSeeding(SCHEDULE));
+console.log("Calculating current seeding...");
+const seeding = getSeeding(SCHEDULE);
 
 const playoffProbabilities = calculatePlayoffProbability(SCHEDULE);
 
-Object.entries(SCHEDULE).forEach(([team, weeks]) => {
-  const opponents = weeks.map((week) => week?.opponent ?? "BYE").join(" ");
-  const wins = weeks.filter((week) => week?.won === true).length;
-  const losses = weeks.filter((week) => week?.won === false).length;
-  const draws = 0; // TODO
-  console.log(
-    team,
-    `${wins}-${losses}-${draws}`,
-    `${playoffProbabilities[team]!.toFixed(2)}%`
-  );
-});
+function drawTable(teams: Team[]) {
+  const table = new Table({
+    head: ["Team", "Record", "Playoff Probability"],
+  });
+
+  teams.forEach((team) => {
+    const weeks = SCHEDULE[team.shorthand]!;
+    const wins = weeks.filter((week) => week?.won === true).length;
+    const losses = weeks.filter((week) => week?.won === false).length;
+    const draws = 0; // TODO
+    table.push([
+      team.name,
+      `${wins}-${losses}-${draws}`,
+      `${playoffProbabilities[team.shorthand]!.toFixed(2)}%`,
+    ]);
+  });
+
+  console.log(table.toString());
+}
+
+const afc = Object.values(TEAM_MAP)
+  .filter((team) => team.conference === Conference.AFC)
+  .sort((a, b) => {
+    const aIndex = seeding.afc.indexOf(a.shorthand);
+    const bIndex = seeding.afc.indexOf(b.shorthand);
+    if (aIndex === -1 && bIndex === -1) {
+      return (
+        playoffProbabilities[b.shorthand]! - playoffProbabilities[a.shorthand]!
+      );
+    } else if (aIndex === -1) {
+      return 1;
+    } else if (bIndex === -1) {
+      return -1;
+    }
+    return aIndex - bIndex;
+  });
+
+const nfc = Object.values(TEAM_MAP)
+  .filter((team) => team.conference === Conference.NFC)
+  .sort((a, b) => {
+    const aIndex = seeding.nfc.indexOf(a.shorthand);
+    const bIndex = seeding.nfc.indexOf(b.shorthand);
+    if (aIndex === -1 && bIndex === -1) {
+      return (
+        playoffProbabilities[b.shorthand]! - playoffProbabilities[a.shorthand]!
+      );
+    } else if (aIndex === -1) {
+      return 1;
+    } else if (bIndex === -1) {
+      return -1;
+    }
+    return aIndex - bIndex;
+  });
+
+drawTable(afc);
+drawTable(nfc);
