43f2c35d324bee598d10e05731c5fed808ab3beb
Frederik Bolding <frederik.bolding@gmail.com>
2026-01-06 16:33:12 +0100

    Use HashRouter

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -4,16 +4,16 @@ import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
 import { DataProvider } from "./worker/DataContext.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
         <DataProvider>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </DataProvider>
       </ThemeProvider>
     </ChakraProvider>
